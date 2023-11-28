295cec3cd7a90187d518f7553ac60853bf91ed05
GAPIT-MINHBV\MinhBV <minhbv@gapit.com.vn>
2023-11-28 13:48:44 +0700

    update useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
- 
+import { useState } from "react";
+
+const orders=[100,200,300];
+
 
 function App() {
+const total=orders.reduce((total,cur) => total+cur)
+  const [counter,setCounter]=useState(total);
+  const handleIncrease=()=>{
+    //setCounter(counter+1)
+    //setCounter(prevState=>prevState+1);
+    //setCounter(prevState=>prevState+1);
+    setCounter(prevState=>prevState+1);
+  }
+
+  const [infor,setInfo]=useState({
+    name:'Nguyễn Văn A',
+    age:12,
+    address:'Ha Noi - VN'
+  })
+  const handleUpdate=()=>{
+    setInfo({
+      ...infor,
+      bio:"Test"
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding:20}}>
+      <h1>{counter}</h1>
+      
+      <button onClick={handleIncrease}>Increase</button>
+      <h1>{JSON.stringify(infor)}</h1>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
