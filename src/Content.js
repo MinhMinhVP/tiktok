8723177ed95abbb6deedc5de460e0dcddaef3180
GAPIT-MINHBV\MinhBV <minhbv@gapit.com.vn>
2023-11-30 14:40:48 +0700

    cm

--- src/Content.js
+++ src/Content.js
@@ -9,14 +9,16 @@ import { useEffect,useState   } from "react"
     // - callback sẽ được lại mỗi khi deps thay đổi
     // ---
     // 1. callback luôn được gọi sau khi componeent mounted (luôn đúng cả  3 trường hơp)
+    // 2. Cleanup function luôn được gọi trước khi component unmounted (luôn đúng cả  3 trường hơp)
     
     const tabs=['posts','comments','albums']
 function Content(){
    const [title,setTitle]=useState('')
    const [posts,setPosts]=useState([])
    const [type,setType]=useState('posts')
-
-    console.log(type);
+    const[showGoToTop,setShowGoToTop]=useState(false)
+    const [width,setWidth]=useState(window.innerWidth)
+    //console.log(type);
     
     useEffect(()=>{
         //console.log('Mounted')
@@ -28,9 +30,43 @@ function Content(){
         })
     },[type])
    
+    useEffect(()=>{
+        const handleScroll=()=>{
+            console.log(window.scrollY);
+            if(window.scrollY >=200){
+                // show
+                setShowGoToTop(true)
+            }else{
+                //hide
+                setShowGoToTop(false)
+            }
+        }
+        window.addEventListener('scroll',handleScroll)
+        //console.log('mounting....');
+
+
+        //Cleanup function
+        return ()=>{
+            //console.log('Umounting....');
+            window.removeEventListener('scroll',handleScroll)
+        }
+    },[])
+
+    useEffect(()=>{
+        const handleResize=()=>{
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize',handleResize)
+
+        //Cleanup function
+        return()=>{
+            window.removeEventListener('resize',handleResize)
+        }
+    },[])
+
     return (
         <div>
-
+            <h1>{width}</h1>
             <input
                 value={title}
                 onChange={e=>setTitle(e.target.value)}
@@ -54,6 +90,15 @@ function Content(){
                     ))
                 }
             </ul>
+            {showGoToTop && (
+                <button
+                    style={{
+                        position:'fixed',
+                        right:20,
+                        bottom:20
+                    }}
+                >Go to top</button>
+            )}
         </div>
     )
 }
